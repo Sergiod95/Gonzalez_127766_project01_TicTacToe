3e94c933b95c483c1bfd33d30163556459209008
Sergiod95 <71178299+Sergiod95@users.noreply.github.com>
2020-09-18 20:58:29 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,32 +1,5 @@
-def main():
-    win = GraphWin('Graphics Window',300,300)
-    wcircle = Circle(Point(150, 150), 100)
-    wcircle.setOutline("black")
-    wcircle.setFill("white")
-    wcircle.draw(win)
-    
-    bcircle = Circle(Point(150,150), 80)
-    bcircle.setOutline("black")
-    bcircle.setFill("black")
-    bcircle.draw(win)
-    
-    blcircle = Circle(Point(150, 150), 60)
-    blcircle.setOutline("black")
-    blcircle.setFill("blue")
-    blcircle.draw(win)
-    
-    rcircle = Circle(Point(150, 150), 40)
-    rcircle.setOutline("black")
-    rcircle.setFill("red")
-    rcircle.draw(win)
-    
-    ycircle = Circle(Point(150, 150), 20)
-    ycircle.setOutline("black")
-    ycircle.setFill("yellow")
-    ycircle.draw(win)
-    pass
-
-main()from graphics import *
+from graphics import *
+from random import *
 
 def main():
     win = GraphWin('Graphics Window',300,300)
@@ -42,30 +15,10 @@ def main():
     HoriL.draw(win)
     Horir.draw(win)
     
-    #a1 = Text(Point(50,50), "a1")
-    ###a2 = Text(Point(50,150), "a2")
-    ##a3 = Text(Point(50,250), "a3")
-    #b1 = Text(Point(150,50), "b1")
-    #b2 = Text(Point(150,150), "b2")
-    #b3 = Text(Point(150,250), "b3")
-    #c1 = Text(Point(250,50), "c1")
-    #c2 = Text(Point(250,150), "c2")
-    #c3 = Text(Point(250,250), "c3")
-    
-    #a1.draw(win)
-    #a2.draw(win)
-    #a3.draw(win)
-    #b1.draw(win)
-    #b2.draw(win)
-    #b3.draw(win)
-    #c1.draw(win)
-    #c2.draw(win)
-    #c3.draw(win)
-    
-    
     grid = [[Text(Point(50,50), "a1"),Text(Point(50,150), "a2"),Text(Point(50,250), "a3")],[Text(Point(150,50), "b1"),Text(Point(150,150), "b2"),Text(Point(150,250), "b3")],[Text(Point(250,50), "c1"),Text(Point(250,150), "c2"),Text(Point(250,250), "c3")]]
     
-    for i in range(9):
+    for i in range(6):
+        #player Turn
         wrongMove = True
         while wrongMove==True:
             player = win.getMouse()
@@ -86,16 +39,89 @@ def main():
             else:
                 Y=2
             
-            if grid[X][Y].getText()=='O':
+            if grid[X][Y].getText()=='O' or grid[X][Y].getText()=='X':
                 wrongMove=True
             else:
                 grid[X][Y].setText("O")
                 grid[X][Y].draw(win)
                 wrongMove=False
         
+        #check if player Won
+        pWin = False
+        if grid[0][0].getText()=='O' and grid[1][0].getText()=='O' and grid[2][0].getText()=='O':
+            pWin = True
+        if grid[0][1].getText()=='O' and grid[1][1].getText()=='O' and grid[2][1].getText()=='O':
+            pWin = True
+        if grid[0][2].getText()=='O' and grid[1][2].getText()=='O' and grid[2][2].getText()=='O':
+            pWin = True
+        if grid[0][0].getText()=='O' and grid[0][1].getText()=='O' and grid[0][2].getText()=='O':
+            pWin = True
+        if grid[1][0].getText()=='O' and grid[1][1].getText()=='O' and grid[1][2].getText()=='O':
+            pWin = True
+        if grid[2][0].getText()=='O' and grid[2][1].getText()=='O' and grid[2][2].getText()=='O':
+            pWin = True
+        if grid[0][0].getText()=='O' and grid[1][1].getText()=='O' and grid[2][2].getText()=='O':
+            pWin = True
+        if grid[2][0].getText()=='O' and grid[1][1].getText()=='O' and grid[0][2].getText()=='O':
+            pWin = True
+        else:
+            pass
+        if pWin == True:
+            end = Text(Point(150,150), "Player Win")
+            end.setSize(20)
+            end.setTextColor("red")
+            end.draw(win)
+            p = win.getMouse()
+            win.close()
+            return 0
+        else: 
+            pass
+        
+        #npc Turn
+        npcWrong=True
+        while npcWrong==True:
+            npcX = randrange(3)
+            npcY = randrange(3)
+            
+            if grid[npcX][npcY].getText()=='O' or grid[npcX][npcY].getText()=='X':
+                npcWrong=True
+            else:
+                grid[npcX][npcY].setText("X")
+                grid[npcX][npcY].draw(win)
+                npcWrong=False
+                
+        #check if NPC Won
+        npcWin = False
+        if grid[0][0].getText()=='X' and grid[1][0].getText()=='X' and grid[2][0].getText()=='X':
+            npcWin = True
+        if grid[0][1].getText()=='X' and grid[1][1].getText()=='X' and grid[2][1].getText()=='X':
+            npcWin = True
+        if grid[0][2].getText()=='X' and grid[1][2].getText()=='X' and grid[2][2].getText()=='X':
+            npcWin = True
+        if grid[0][0].getText()=='X' and grid[0][1].getText()=='X' and grid[0][2].getText()=='X':
+            npcWin = True
+        if grid[1][0].getText()=='X' and grid[1][1].getText()=='X' and grid[1][2].getText()=='X':
+            npcWin = True
+        if grid[2][0].getText()=='X' and grid[2][1].getText()=='X' and grid[2][2].getText()=='X':
+            npcWin = True
+        if grid[0][0].getText()=='X' and grid[1][1].getText()=='X' and grid[2][2].getText()=='X':
+            npcWin = True
+        if grid[2][0].getText()=='X' and grid[1][1].getText()=='X' and grid[0][2].getText()=='X':
+            npcWin = True
+        else:
+            pass
+        if npcWin == True:
+            end = Text(Point(150,150), "Computer Win")
+            end.setSize(20)
+            end.setTextColor("red")
+            end.draw(win)
+            p = win.getMouse()
+            win.close()
+            return 0
+        else: 
+            pass
+                
+                
     
-    a = win.getMouse()
-    win.close()
-    pass
 
 main()
