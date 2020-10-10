eae1282d422d4c2cea47a35a7efd0b502cb96e45
Sergiod95 <71178299+Sergiod95@users.noreply.github.com>
2020-10-09 23:59:46 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
-  
 from graphics import *
 from random import *
+
+
 
 def match_screen(win,grid):
     #grid
@@ -18,6 +19,115 @@ def match_screen(win,grid):
         for j in range(3):
             grid[i][j].setSize(30)  
     pass
+
+def reset_screen(win,grid):
+
+    for i in range(3):
+        for j in range(3):
+            grid[i][j].setText("empty")
+            grid[i][j].undraw()  
+    pass
+
+def menu_dif(win,dificulty):
+    
+    r = Rectangle(Point(0.5,0.75),Point(2.5,2.25))
+    r.setFill("Grey")
+    r.setOutline("Black")
+    r.draw(win)
+    menu = Text(Point(1.5, 1),"Dificulty")
+    menu.draw(win)
+
+    easy = Rectangle(Point(0.65,1.5),Point(1.15,2))
+    easy .setFill("White")
+    easy .setOutline("Black")
+    normal = Rectangle(Point(1.25,1.5),Point(1.75,2))
+    normal .setFill("White")
+    normal .setOutline("Black")
+    hard = Rectangle(Point(1.85,1.5),Point(2.35,2))
+    hard .setFill("White")
+    hard .setOutline("Black")
+    easy.draw(win)
+    normal.draw(win)
+    hard.draw(win)
+
+    easpoint = easy.getCenter()
+    norpoint = normal.getCenter()
+    harpoint = hard.getCenter()
+    
+    eas = Text(easpoint,"Easy")
+    eas.setTextColor("Black")
+    eas.draw(win)
+    norml = Text(norpoint,"Normal")
+    norml.setTextColor("Black")
+    norml.draw(win)
+    har = Text(harpoint,"Hard")
+    har.setTextColor("Black")
+    har.draw(win)
+
+    while True:
+        p = win.getMouse()
+        if p.getX()>0.65 and p.getX()<1.15 and p.getY()>1.5 and p.getY()<2:
+            dificulty = 0
+            break
+        elif p.getX()>1.25 and p.getX()<1.75 and p.getY()>1.5 and p.getY()<2:
+            dificulty = 1
+            break
+        elif p.getX()>1.85 and p.getX()<2.15 and p.getY()>1.5 and p.getY()<2:
+            dificulty = 2
+            break
+    r.undraw()
+    menu.undraw()
+    easy.undraw()
+    normal.undraw()
+    hard.undraw()
+    eas.undraw()
+    norml.undraw()
+    har.undraw()
+    return dificulty
+
+def rematch(win,contin_playing):
+    
+    r = Rectangle(Point(0.5,0.75),Point(2.5,2.25))
+    r.setFill("Grey")
+    r.setOutline("Black")
+    r.draw(win)
+    rematch = Text(Point(1.5, 1),"Rematch?")
+    rematch.draw(win)
+
+    y_b = Rectangle(Point(0.75,1.5),Point(1.25,2))
+    y_b .setFill("White")
+    y_b .setOutline("Black")
+    n_b = Rectangle(Point(1.75,1.5),Point(2.25,2))
+    n_b .setFill("White")
+    n_b .setOutline("Black")
+    y_b.draw(win)
+    n_b.draw(win)
+
+    ypoint = y_b.getCenter()
+    npoint = n_b.getCenter()
+    
+    yes = Text(ypoint,"Yes")
+    yes.setTextColor("Black")
+    yes.draw(win)
+    no = Text(npoint,"No")
+    no.setTextColor("Black")
+    no.draw(win)
+
+    while True:
+        p = win.getMouse()
+        if p.getX()>0.75 and p.getX()<1.25 and p.getY()>1.5 and p.getY()<2:
+            contin_playing = "yes"
+            break
+        elif p.getX()>1.75 and p.getX()<2.25 and p.getY()>1.5 and p.getY()<2:
+            contin_playing = "no"
+            break
+    r.undraw()
+    rematch.undraw()
+    y_b.undraw()
+    n_b.undraw()
+    yes.undraw()
+    no.undraw()
+    return contin_playing
 
 def player_turn(win,grid,i):
     #player Turn
@@ -79,7 +189,7 @@ def win_check( o_x , p_npc, i,win , grid):
         end.setTextColor("red")
         end.draw(win)
         p = win.getMouse()
-        win.close()
+        end.undraw()
         return 1
     elif i == 4: 
         end = Text(Point(1.5,1.5), "Draw")
@@ -87,12 +197,12 @@ def win_check( o_x , p_npc, i,win , grid):
         end.setTextColor("red")
         end.draw(win)
         p = win.getMouse()
-        win.close()
+        end.undraw()
         return 1
     return 0
 
-def computer_turn(win,grid,i):
-    if i < 1:
+def computer_turn(win,grid,i,dificulty):
+    if i ==0 or dificulty==0:
         npcWrong=True
         while npcWrong==True:
             npcX = randrange(3)
@@ -185,92 +295,8 @@ def computer_turn(win,grid,i):
         elif grid[2][0].getText()=='X' and grid[1][1].getText()=='X' and grid[0][2].getText()!='O':
             grid[0][2].setText("X")
             grid[0][2].draw(win)
-        #check if computer won at this point
-        win_game = win_check( 'X' , "Computer", i ,win , grid)
-        if win_game == 1:
-            return 1
 
-    #AI countering player move
-        if grid[0][0].getText()!='X' and grid[1][0].getText()=='O' and grid[2][0].getText()=='O':
-            grid[0][0].setText("X")
-            grid[0][0].draw(win)
-        elif grid[0][0].getText()=='O' and grid[1][0].getText()!='X' and grid[2][0].getText()=='O':
-            grid[1][0].setText("X")
-            grid[1][0].draw(win)
-        elif grid[0][0].getText()=='O' and grid[1][0].getText()=='O' and grid[2][0].getText()!='X':
-            grid[2][0].setText("X")
-            grid[2][0].draw(win)
-                
-        elif grid[0][1].getText()!='X' and grid[1][1].getText()=='O' and grid[2][1].getText()=='O':
-            grid[0][1].setText("X")
-            grid[0][1].draw(win)
-        elif grid[0][1].getText()=='O' and grid[1][1].getText()!='X' and grid[2][1].getText()=='O':
-            grid[1][1].setText("X")
-            grid[1][1].draw(win)
-        elif grid[0][1].getText()=='O' and grid[1][1].getText()=='O' and grid[2][1].getText()!='X':
-            grid[2][1].setText("X")
-            grid[2][1].draw(win)
-                
-        elif grid[0][2].getText()!='X' and grid[1][2].getText()=='O' and grid[2][2].getText()=='O':
-            grid[0][2].setText("X")
-            grid[0][2].draw(win)
-        elif grid[0][2].getText()=='O' and grid[1][2].getText()!='X' and grid[2][2].getText()=='O':
-            grid[1][2].setText("X")
-            grid[1][2].draw(win)
-        elif grid[0][2].getText()=='O' and grid[1][2].getText()=='O' and grid[2][2].getText()!='X':
-            grid[2][2].setText("X")
-            grid[2][2].draw(win)
-                
-        elif grid[0][0].getText()!='X' and grid[0][1].getText()=='O' and grid[0][2].getText()=='O':
-            grid[0][0].setText("X")
-            grid[0][0].draw(win)
-        elif grid[0][0].getText()=='O' and grid[0][1].getText()!='X' and grid[0][2].getText()=='O':
-            grid[0][1].setText("X")
-            grid[0][1].draw(win)
-        elif grid[0][0].getText()=='O' and grid[0][1].getText()=='O' and grid[0][2].getText()!='X':
-            grid[0][2].setText("X")
-            grid[0][2].draw(win)
-                
-        elif grid[1][0].getText()!='X' and grid[1][1].getText()=='O' and grid[1][2].getText()=='O':
-            grid[1][0].setText("X")
-            grid[1][0].draw(win)
-        elif grid[1][0].getText()=='O' and grid[1][1].getText()!='X' and grid[1][2].getText()=='O':
-            grid[1][1].setText("X")
-            grid[1][1].draw(win)
-        elif grid[1][0].getText()=='O' and grid[1][1].getText()=='O' and grid[1][2].getText()!='X':
-            grid[1][2].setText("X")
-            grid[1][2].draw(win)
-                
-        elif grid[2][0].getText()!='X' and grid[2][1].getText()=='O' and grid[2][2].getText()=='O':
-            grid[2][0].setText("X")
-            grid[2][0].draw(win)
-        elif grid[2][0].getText()=='O' and grid[2][1].getText()!='X' and grid[2][2].getText()=='O':
-            grid[2][1].setText("X")
-            grid[2][1].draw(win)
-        elif grid[2][0].getText()=='O' and grid[2][1].getText()=='O' and grid[2][2].getText()!='X':
-            grid[2][2].setText("X")
-            grid[2][2].draw(win)
-                
-        elif grid[0][0].getText()!='X' and grid[1][1].getText()=='O' and grid[2][2].getText()=='O':
-            grid[0][0].setText("X")
-            grid[0][0].draw(win)
-        elif grid[0][0].getText()=='O' and grid[1][1].getText()!='X' and grid[2][2].getText()=='O':
-            grid[1][1].setText("X")
-            grid[1][1].draw(win)
-        elif grid[0][0].getText()=='O' and grid[1][1].getText()=='O' and grid[2][2].getText()!='X':
-            grid[2][2].setText("X")
-            grid[2][2].draw(win)
-                
-        elif grid[2][0].getText()!='X' and grid[1][1].getText()=='O' and grid[0][2].getText()=='O':
-            grid[2][0].setText("X")
-            grid[2][0].draw(win)
-        elif grid[2][0].getText()=='O' and grid[1][1].getText()!='X' and grid[0][2].getText()=='O':
-            grid[1][1].setText("X")
-            grid[1][1].draw(win)
-        elif grid[2][0].getText()=='O' and grid[1][1].getText()=='O' and grid[0][2].getText()!='X':
-            grid[0][2].setText("X")
-            grid[0][2].draw(win)
-        else:
+        elif dificulty == 1:
             if i > 0:
                 npcWrong=True
                 while npcWrong==True:
@@ -283,6 +309,106 @@ def computer_turn(win,grid,i):
                         grid[npcX][npcY].setText("X")
                         grid[npcX][npcY].draw(win)
                         npcWrong=False
+        
+        #check if computer won at this point
+        win_game = win_check( 'X' , "Computer", i ,win , grid)
+        if win_game == 1:
+            return 1
+
+    #AI countering player move
+        if dificulty == 2:
+            if grid[0][0].getText()!='X' and grid[1][0].getText()=='O' and grid[2][0].getText()=='O':
+                grid[0][0].setText("X")
+                grid[0][0].draw(win)
+            elif grid[0][0].getText()=='O' and grid[1][0].getText()!='X' and grid[2][0].getText()=='O':
+                grid[1][0].setText("X")
+                grid[1][0].draw(win)
+            elif grid[0][0].getText()=='O' and grid[1][0].getText()=='O' and grid[2][0].getText()!='X':
+                grid[2][0].setText("X")
+                grid[2][0].draw(win)
+                
+            elif grid[0][1].getText()!='X' and grid[1][1].getText()=='O' and grid[2][1].getText()=='O':
+                grid[0][1].setText("X")
+                grid[0][1].draw(win)
+            elif grid[0][1].getText()=='O' and grid[1][1].getText()!='X' and grid[2][1].getText()=='O':
+                grid[1][1].setText("X")
+                grid[1][1].draw(win)
+            elif grid[0][1].getText()=='O' and grid[1][1].getText()=='O' and grid[2][1].getText()!='X':
+                grid[2][1].setText("X")
+                grid[2][1].draw(win)
+                
+            elif grid[0][2].getText()!='X' and grid[1][2].getText()=='O' and grid[2][2].getText()=='O':
+                grid[0][2].setText("X")
+                grid[0][2].draw(win)
+            elif grid[0][2].getText()=='O' and grid[1][2].getText()!='X' and grid[2][2].getText()=='O':
+                grid[1][2].setText("X")
+                grid[1][2].draw(win)
+            elif grid[0][2].getText()=='O' and grid[1][2].getText()=='O' and grid[2][2].getText()!='X':
+                grid[2][2].setText("X")
+                grid[2][2].draw(win)
+                
+            elif grid[0][0].getText()!='X' and grid[0][1].getText()=='O' and grid[0][2].getText()=='O':
+                grid[0][0].setText("X")
+                grid[0][0].draw(win)
+            elif grid[0][0].getText()=='O' and grid[0][1].getText()!='X' and grid[0][2].getText()=='O':
+                grid[0][1].setText("X")
+                grid[0][1].draw(win)
+            elif grid[0][0].getText()=='O' and grid[0][1].getText()=='O' and grid[0][2].getText()!='X':
+                grid[0][2].setText("X")
+                grid[0][2].draw(win)
+                
+            elif grid[1][0].getText()!='X' and grid[1][1].getText()=='O' and grid[1][2].getText()=='O':
+                grid[1][0].setText("X")
+                grid[1][0].draw(win)
+            elif grid[1][0].getText()=='O' and grid[1][1].getText()!='X' and grid[1][2].getText()=='O':
+                grid[1][1].setText("X")
+                grid[1][1].draw(win)
+            elif grid[1][0].getText()=='O' and grid[1][1].getText()=='O' and grid[1][2].getText()!='X':
+                grid[1][2].setText("X")
+                grid[1][2].draw(win)
+                
+            elif grid[2][0].getText()!='X' and grid[2][1].getText()=='O' and grid[2][2].getText()=='O':
+                grid[2][0].setText("X")
+                grid[2][0].draw(win)
+            elif grid[2][0].getText()=='O' and grid[2][1].getText()!='X' and grid[2][2].getText()=='O':
+                grid[2][1].setText("X")
+                grid[2][1].draw(win)
+            elif grid[2][0].getText()=='O' and grid[2][1].getText()=='O' and grid[2][2].getText()!='X':
+                grid[2][2].setText("X")
+                grid[2][2].draw(win)
+                
+            elif grid[0][0].getText()!='X' and grid[1][1].getText()=='O' and grid[2][2].getText()=='O':
+                grid[0][0].setText("X")
+                grid[0][0].draw(win)
+            elif grid[0][0].getText()=='O' and grid[1][1].getText()!='X' and grid[2][2].getText()=='O':
+                grid[1][1].setText("X")
+                grid[1][1].draw(win)
+            elif grid[0][0].getText()=='O' and grid[1][1].getText()=='O' and grid[2][2].getText()!='X':
+                grid[2][2].setText("X")
+                grid[2][2].draw(win)
+                
+            elif grid[2][0].getText()!='X' and grid[1][1].getText()=='O' and grid[0][2].getText()=='O':
+                grid[2][0].setText("X")
+                grid[2][0].draw(win)
+            elif grid[2][0].getText()=='O' and grid[1][1].getText()!='X' and grid[0][2].getText()=='O':
+                grid[1][1].setText("X")
+                grid[1][1].draw(win)
+            elif grid[2][0].getText()=='O' and grid[1][1].getText()=='O' and grid[0][2].getText()!='X':
+                grid[0][2].setText("X")
+                grid[0][2].draw(win)
+            elif dificulty == 2:
+                if i > 0:
+                    npcWrong=True
+                    while npcWrong==True:
+                        npcX = randrange(3)
+                        npcY = randrange(3)
+                
+                        if grid[npcX][npcY].getText()=='O' or grid[npcX][npcY].getText()=='X':
+                            npcWrong=True
+                        else:
+                            grid[npcX][npcY].setText("X")
+                            grid[npcX][npcY].draw(win)
+                            npcWrong=False
             
     #check if NPC Won part 2
     win_game = win_check( 'X' , "Computer", i ,win , grid)
@@ -290,23 +416,36 @@ def computer_turn(win,grid,i):
         return 1
     return 0
 
-
 def main():
     win = GraphWin('Graphics Window',300,300)
     win.setCoords(0, 3, 3, 0)
     grid = [[Text(Point(0.5,0.5), "a1"),Text(Point(0.5,1.5), "a2"),Text(Point(0.5,2.5), "a3")],[Text(Point(1.5,0.5), "b1"),Text(Point(1.5,1.5), "b2"),Text(Point(1.5,2.5), "b3")],[Text(Point(2.5,0.5), "c1"),Text(Point(2.5,1.5), "c2"),Text(Point(2.5,2.5), "c3")]]
+    contin_playing = "yes"
     #creates the grid for the game
-    match_screen(win,grid)
+    number_matches = 0
+    dificulty = 0
     
-    for i in range(6):
-        #player Turn
-        turn = player_turn(win,grid, i)
-        if turn == 1:
-            return 1
+    while(contin_playing=="yes"):
         
-        #npc Turn
-        turn = computer_turn(win,grid, i)
-        if turn == 1:
-            return 1
+        dificulty = menu_dif(win,dificulty)
+        if number_matches > 0:
+            reset_screen(win,grid)
+            
+        number_matches +=1
+        match_screen(win,grid)
+        for i in range(6):
+            #player Turn
+            turn = player_turn(win,grid, i)
+            if turn == 1:
+                contin_playing = rematch(win,contin_playing)
+                break
+        
+            #npc Turn
+            turn = computer_turn(win,grid, i,dificulty)
+            if turn == 1:
+                contin_playing = rematch(win,contin_playing)
+                break
+    p = win.getMouse()
+    win.close()
 
 main()
